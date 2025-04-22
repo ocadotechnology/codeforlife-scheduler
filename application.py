c7e2851ae6bc7edc4613deea7121efe53978463b
Stefan Kairinos <skairinos@gmail.com>
2025-04-22 15:51:21 +0100

    Initial commit

---
+++ application.py
@@ -0,0 +1,14 @@
+"""
+© Ocado Group
+Created on 11/04/2024 at 16:51:45(+01:00).
+
+The entrypoint to our app.
+"""
+
+from codeforlife.servers import CeleryServer, DjangoServer
+
+DjangoServer.setup()
+
+celery_app = CeleryServer().app
+
+django_app = DjangoServer().wsgi_app
