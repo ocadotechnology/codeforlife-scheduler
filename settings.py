17b49a05b220e3eb883c1ff11864cf8aec22a185
SKairinos <skairinos@gmail.com>
2025-06-06 16:32:27 +0000

    fix: configure celery

--- settings.py
+++ settings.py
@@ -3,14 +3,10 @@
 Created on 22/04/2025 at 16:05:09(+01:00).
 """
 
-import typing as t
 from pathlib import Path
 
 from codeforlife import set_up_settings
 from src.utils import configure_celery
-
-if t.TYPE_CHECKING:
-    from src.types import TaskRoutes, TaskSchedules
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
@@ -21,15 +17,5 @@ secrets = set_up_settings(BASE_DIR, service_name="scheduler")
 from codeforlife.settings import *
 
 SECRET_KEY = secrets.SECRET_KEY
-
-# Celery
-# https://docs.celeryq.dev/en/v5.4.0/userguide/configuration.html
-# https://docs.celeryq.dev/en/v5.4.0/getting-started/backends-and-brokers/sqs.html
-
-# WARN: Do not set - leave empty.
-# These dictionaries are populated at runtime by a utility function.
-CELERY_BROKER_TRANSPORT_OPTIONS["predefined_queues"] = {}
-CELERY_BEAT_SCHEDULE: "TaskSchedules" = {}
-CELERY_TASK_ROUTES: "TaskRoutes" = {}
 
 configure_celery()
--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ from codeforlife.tasks import get_local_sqs_url
 from .schedules import SCHEDULES
 
 if t.TYPE_CHECKING:
-    from .types import PredefinedQueues
+    from .types import PredefinedQueues, TaskRoutes, TaskSchedules
 
 
 def configure_celery():
@@ -20,37 +20,42 @@ def configure_celery():
     2. Task schedules.
     3. Task routes.
 
+    https://docs.celeryq.dev/en/v5.4.0/userguide/configuration.html
+    https://docs.celeryq.dev/en/v5.4.0/getting-started/backends-and-brokers/sqs.html
+
     Args:
         service_task_schedules: The name and task schedules for each service.
     """
 
     # pylint: disable-next=import-outside-toplevel
-    from settings import (  # type: ignore[attr-defined]
-        AWS_REGION,
-        CELERY_BEAT_SCHEDULE,
-        CELERY_BROKER_TRANSPORT_OPTIONS,
-        CELERY_TASK_ROUTES,
-        ENV,
-    )
+    import settings
 
-    predefined_queues = t.cast(
-        "PredefinedQueues", CELERY_BROKER_TRANSPORT_OPTIONS["predefined_queues"]
-    )
+    predefined_queues: "PredefinedQueues" = {}
+    beat_schedule: "TaskSchedules" = {}
+    task_routes: "TaskRoutes" = {}
 
     for service, task_schedules in SCHEDULES.items():
         # Add beat schedule.
         for name, task_schedule in task_schedules.items():
             task_schedule["task"] = f"{service}.{task_schedule['task']}"
-            CELERY_BEAT_SCHEDULE[f"{service}.{name}"] = task_schedule
+            beat_schedule[f"{service}.{name}"] = task_schedule
 
         # Add task route.
-        CELERY_TASK_ROUTES[f"{service}.*"] = {"queue": service}
+        task_routes[f"{service}.*"] = {"queue": service}
 
         # Add predefined queue.
         predefined_queues[service] = {
             "url": (
                 os.environ[f"{service.replace('-', '_').upper()}_SQS_URL"]
-                if ENV != "local"
-                else get_local_sqs_url(t.cast(str, AWS_REGION), service)
+                if settings.ENV != "local"
+                else get_local_sqs_url(
+                    t.cast(str, settings.AWS_REGION), service
+                )
             )
         }
+
+    settings.CELERY_BROKER_TRANSPORT_OPTIONS["predefined_queues"] = (
+        predefined_queues  # type: ignore[assignment]
+    )
+    settings.CELERY_BEAT_SCHEDULE = beat_schedule  # type: ignore[attr-defined]
+    settings.CELERY_TASK_ROUTES = task_routes  # type: ignore[attr-defined]
